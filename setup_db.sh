26442e0c920f456ea93f0e2afc27426916d64c7b
Vuk Mirovic <wooque@gmail.com>
2016-09-24 21:40:25 +0200

    Move init DB script to project root

--- setup_db.sh
+++ setup_db.sh
@@ -2,4 +2,4 @@
 sudo -u postgres createdb bench
 sudo -u postgres psql -c "CREATE USER bench WITH PASSWORD 'bench'"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE bench TO bench"
-./init_db.py
+sudo -u postgres psql bench -c "CREATE TABLE IF NOT EXISTS tst (id SERIAL, txt TEXT)"
