02edb1f8fdadbe7cc96be0f7bbed1f8a54877288
Vuk Mirovic <wooque@gmail.com>
2016-10-31 19:12:00 +0100

    Replace aiohttp example with sanic/uvloop

--- python/bench_aio.py
+++ python/bench_aio.py
@@ -1,60 +1,61 @@
 from random import randint
 import os
 import asyncio
-from aiohttp import web
+import uvloop
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 import aiopg
+from sanic import Sanic
+from sanic.response import json
 
 trans = bytes.maketrans(bytearray(range(256)),
                         bytearray([ord(b'a') + b % 26 for b in range(256)]))
 
+
 def randstr(size):
     return os.urandom(size).translate(trans).decode('ascii')
 
+pool = None
 
-class Handler:
+async def create_pool():
+    global pool
+    dsn = 'host=127.0.0.1 dbname=bench user=bench password=bench'
+    pool = await aiopg.create_pool(dsn, minsize=32, maxsize=32)
+    
+asyncio.get_event_loop().run_until_complete(create_pool())
 
-    def __init__(self):
-        self.pool = None
-        
-        async def create_pool():
-            dsn = 'host=127.0.0.1 dbname=bench user=bench password=bench'
-            self.pool = await aiopg.create_pool(dsn, minsize=32, maxsize=32)
-            
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(create_pool())
+app = Sanic(__name__)
 
-    async def handle(self, request):
-        coin = randint(0, 9)
-        async with self.pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                if coin < 6:
-                    await cur.execute("SELECT title, thumb, nc, nv FROM tst ORDER BY id DESC LIMIT 10")
-                    result = []
-                    async for row in cur:
-                        result.append(dict(title=row[0], thumb=row[1], nc=row[2], nv=row[3]))
-                    resp = dict(list=result)
 
-                elif coin < 8:
-                    title = randstr(140)
-                    thumb = randstr(140)
-                    nc = randint(0, 1000)
-                    nv = randint(0, 5000)
-                    await cur.execute("INSERT INTO tst(title, thumb, nc, nv) VALUES (%s, %s, %s, %s)", 
-                                      (title, thumb, nc, nv,))
-                    resp = dict(action="insert", title=title, thumb=thumb, nc=nc, nv=nv)
+@app.route("/")
+async def handle(request):
+    coin = randint(0, 9)
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            if coin < 6:
+                await cur.execute("SELECT title, thumb, nc, nv FROM tst ORDER BY id DESC LIMIT 10")
+                result = []
+                async for row in cur:
+                    result.append(dict(title=row[0], thumb=row[1], nc=row[2], nv=row[3]))
+                resp = dict(list=result)
 
-                else:
-                    title = randstr(140)
-                    thumb = randstr(140)
-                    nc = randint(0, 1000)
-                    nv = randint(0, 5000)
-                    await cur.execute("UPDATE tst SET title=%s, thumb=%s, nc=%s, nv=%s "
-                                      "WHERE id=(SELECT max(id) as m FROM tst LIMIT 1)", (title, thumb, nc, nv,))
-                    resp = dict(action="update", title=title, thumb=thumb, nc=nc, nv=nv)
-                          
-        return web.json_response(resp)
+            elif coin < 8:
+                title = randstr(140)
+                thumb = randstr(140)
+                nc = randint(0, 1000)
+                nv = randint(0, 5000)
+                await cur.execute("INSERT INTO tst(title, thumb, nc, nv) VALUES (%s, %s, %s, %s)",
+                                  (title, thumb, nc, nv,))
+                resp = dict(action="insert", title=title, thumb=thumb, nc=nc, nv=nv)
 
-handler = Handler()
-app = web.Application()
-app.router.add_route('GET', '/bench', handler.handle)
-web.run_app(app)
+            else:
+                title = randstr(140)
+                thumb = randstr(140)
+                nc = randint(0, 1000)
+                nv = randint(0, 5000)
+                await cur.execute("UPDATE tst SET title=%s, thumb=%s, nc=%s, nv=%s "
+                                  "WHERE id=(SELECT max(id) as m FROM tst LIMIT 1)", (title, thumb, nc, nv,))
+                resp = dict(action="update", title=title, thumb=thumb, nc=nc, nv=nv)
+
+    return json(resp)
+
+app.run(loop=asyncio.get_event_loop(), port=8080)
