8ef1ca211cbe4e659d39008e4e5123cd5ce11dcf
Vuk Mirovic <wooque@gmail.com>
2016-11-04 11:38:50 +0100

    Update setup_db script

--- setup_db.sh
+++ setup_db.sh
@@ -2,5 +2,5 @@
 sudo -u postgres createdb bench
 sudo -u postgres psql -c "CREATE USER bench WITH PASSWORD 'bench'"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE bench TO bench"
-PG_PASSWORD=bench psql -U bench bench -c "CREATE TABLE IF NOT EXISTS tst (id SERIAL, title TEXT, thumb TEXT, nc INTEGER, nv INTEGER)"
-PG_PASSWORD=bench psql -U bench bench -c "CREATE INDEX ON tst(id desc)"
+PGPASSWORD=bench psql -h 127.0.0.1 -U bench bench -c "CREATE TABLE IF NOT EXISTS tst (id SERIAL, title TEXT, thumb TEXT, nc INTEGER, nv INTEGER)"
+PGPASSWORD=bench psql -h 127.0.0.1 -U bench bench -c "CREATE INDEX ON tst(id desc)"
